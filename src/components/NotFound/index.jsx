dcdc0131daf847ce7cb748e9375c8d998bfb68b2
Lê Minh Toàn <mmminhle771@gmail.com>
2021-11-10 17:00:21 +0700

    not found

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,17 +1,33 @@
-import { Button, Result } from 'antd';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NotFound(props) {
     return (
-        <>
-            <Result
-                status="404"
-                title="404"
-                subTitle="Sorry, the page you visited does not exist."
-                extra={<Button href='/' type="primary">Back Home</Button>}
-            />
-        </>
+        <div style={styles.container}>
+            <h3 style={styles.title}>Sorry, the page you visited does not exist.</h3>
+            <Link to="/" style={styles.link}>Go Home</Link>
+        </div>
     );
+}
+
+const styles = {
+    container: {
+        textAlign: 'center',
+    },
+    title: {
+        fontSize: '1.5rem',
+        color: 'white',
+        textAlign: 'center',
+        margin: '30px 0 10px 0'
+    },
+    link: {
+        textAlign: 'center',
+        fontSize: '16px',
+        display: 'inline-block',
+        backgroundColor: '#3187D3',
+        padding: '10px',
+        borderRadius: '5px',
+    }
 }
 
 export default NotFound;
